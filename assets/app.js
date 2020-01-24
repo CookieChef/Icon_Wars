c2741666f3517fcaccdb4f1b66a4e70efcead972
Sara Martinez <s_martinez_1990@outlook.com>
2020-01-24 15:17:26 -0500

    collapsable button display functionality

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,12 @@ console.log("Thank you for playing!");
 //function that starts the game and contains the logic
 playGame = () =>{ 
     console.log("game start!");
+    $("#example").html("game stuff can go here");
+}
+
+//function that displays the game rules 
+showRules = () =>{
+    console.log("Rules are:");
 }
 
 //function that refreshes the page when clicked. 
