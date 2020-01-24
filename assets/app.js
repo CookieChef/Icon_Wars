cf1e9cd2e7efcef9cf079cab31f0d2c07afcf27a
Sara Martinez <s_martinez_1990@outlook.com>
2020-01-24 17:39:58 -0500

    code clean-up

--- assets/app.js
+++ assets/app.js
@@ -5,20 +5,12 @@ console.log("Thank you for playing!");
 //function that starts the game and contains the logic
 playGame = () =>{ 
     console.log("game start!");
+    //once the user clicks on "play" display the elements
     $("#weapon").prepend("Chose your weapon:" + "<br />");
+    //add the "<img>" tag to each of the following elements "
     $("#sword").append("<img src='assets/images/sword.png' style='width:100%;'>");    
     $("#magic").append("<img src='assets/images/magic.png' style='width:100%;'>");
     $("#bow").append("<img src='assets/images/bow.png' style='width:100%;'>");
-
-    style="text-align: center;"
-    // $("#example").append("second line");
-    // $("#example").append("<br />" + "third line");
-}
-
-
-//function that refreshes the page when clicked. 
-restartGame = () => {
-    location.reload(true);
 }
 
 //user chooses sword as a weapon
@@ -34,6 +26,11 @@ magicSelect = () => {
 //user chooses bow as a weapon
 bowSelect = () => {
     console.log("you chose to use the bow");
+}
+
+//function that refreshes the page when clicked or "quit" game. 
+restartGame = () => {
+    location.reload(true);
 }
 
 //end of document.ready function    
