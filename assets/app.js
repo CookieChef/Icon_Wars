f033b3de990343c9cffe98b4cc8af5cbe1eb314e
Sara Martinez <s_martinez_1990@outlook.com>
2020-01-24 16:53:25 -0500

    debugging image rendering

--- assets/app.js
+++ assets/app.js
@@ -5,9 +5,12 @@ console.log("Thank you for playing!");
 //function that starts the game and contains the logic
 playGame = () =>{ 
     console.log("game start!");
-    $("#example").html("first line" + "<br />");
-    $("#example").append("second line");
-    $("#example").append("<br />" + "third line");
+    $("#weapon").html("Chose your weapon:" + "<br />");
+    // $("#sword").append("<img src='assets/images/sword.png' style='width:100%'>");
+    // $("#magic").append("<img src='assets/images/magic.png' style='width:100%'>");
+    // $("#example").append("second line");
+    // $("#example").append("<br />" + "third line");
+
 }
 
 
@@ -16,9 +19,20 @@ restartGame = () => {
     location.reload(true);
 }
 
+//user chooses sword as a weapon
+swordSelect = () => {
+    console.log("you chose the soword!");
+}
 
+//user chooses magic as a weapon
+magicSelect = () => {
+    console.log("you chose to use magic");
+}
 
-
+//user chooses bow as a weapon
+bowSelect = () => {
+    console.log("you chose to use the bow");
+}
 
 //end of document.ready function    
 })
