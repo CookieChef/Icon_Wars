6fe7f126944d68e83e6dcbdd588665a2b1fa9c62
Sara Martinez <s_martinez_1990@outlook.com>
2020-01-24 17:21:05 -0500

    image DOM manipulation completed

--- assets/app.js
+++ assets/app.js
@@ -5,10 +5,12 @@ console.log("Thank you for playing!");
 //function that starts the game and contains the logic
 playGame = () =>{ 
     console.log("game start!");
-    $("#weapon").html("Chose your weapon:" + "<br />");
+    $("#weapon").prepend("Chose your weapon:" + "<br />");
     $("#sword").append("<img src='assets/images/sword.png' style='width:100%'>");    
     $("#magic").append("<img src='assets/images/magic.png' style='width:100%'>");
     $("#bow").append("<img src='assets/images/bow.png' style='width:100%'>");
+
+    
     // $("#example").append("second line");
     // $("#example").append("<br />" + "third line");
 }
